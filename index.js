ea2ef04b4802a6dc56f75e59e596c40f9980f896
Alfredo <alfredodomin91@gmail.com>
2019-12-13 19:10:44 -0600

    object oriented updated based on class

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
 
-var fs = require('fs'); // loading FileSystem library
-var inquirer = require('inquirer');
-var axios = require("axios");
+const inquirer = require ('inquirer')
+const axios = require ('axios');
 
-inquirer
-    .prompt({
-        message:'Please enter your GitHub username: ',
-        name: "username"
-    })
-    .then(function ({username}) {
-        var qUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        axios.get(qUrl).then(function (res) {   // server side http request 
-            var repo_list = res.data.map(function (data) {
-                return data.name;
-            });
-            var repos_Str = repo_list.join("\n");
-            // console.log(repoNames)
-            fs.writeFile('Usersrepo_list.txt', repos_Str, () => {} ); // writing txt file with repos
-        });
-    });
+class DoMyHomework {
+
+    constructor () {
+        this.githubUserName = null; // properties that will be defined in the future
+        this.color = null;
+        this.location = null; 
+        this.profilePicture = null;
+        this.followers = null;
+        this.numOfRepos = null;
+        this.following = null;
+        this.numberofStars = null;
+
+    }
+
+    promptUser(){
+        return inquirer.prompt([
+            {
+            message: 'What is your Username',
+            name:'githubUserName'
+            }
+        ]).then(answers => {
+            // console.log(this); //operator this refer to newHomework Object
+            this.githubUserName = answers.githubUserName
+            // console.log(this);
+            this.makeApiRequest();
+
+        })
+    }
+    makeApiRequest(){
+        return Promise.all ([axios.get(`https://api.github.com/users/${this.githubUserName}`),axios.get(`https://api.github.com/users/${this.githubUserName}/starred`)]
+)            .then(([{data:{ avatar_url, location, name, blog, bio, public_repos, followers, following,}}, {data}]) => {
+                this.avatar_url = avatar_url;
+                this.location = location;
+                this.name = name;
+                this.blog = blog;
+                this.bio = bio;
+                this.public_repos = public_repos;
+                this.followers = followers;
+                this.following = following;
+                // console.log(this);
+                console.log(data)
+            })
+    }
+    createHthml(){
+
+    }
+    createPdf(){
+
+    }
+
+}
+
+
+var newHomework = new DoMyHomework();
+// console.log(newHomework);
+newHomework.promptUser();
+
+
+
+
+
+
+
