fd5342450823e3331ad92b5f61b7513a2b263b57
Alfredo <alfredodomin91@gmail.com>
2019-12-13 20:22:53 -0600

    missing html formatt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const inquirer = require ('inquirer')
 const axios = require ('axios');
+const pdf = require ('html-pdf')
 
 class DoMyHomework {
 
@@ -21,10 +22,15 @@ class DoMyHomework {
             {
             message: 'What is your Username',
             name:'githubUserName'
+            },
+            {
+            message:'What is your favorite color?',
+            name:'color'  
             }
         ]).then(answers => {
             // console.log(this); //operator this refer to newHomework Object
             this.githubUserName = answers.githubUserName
+            this.color = answers.color
             // console.log(this);
             this.makeApiRequest();
 
@@ -32,7 +38,7 @@ class DoMyHomework {
     }
     makeApiRequest(){
         return Promise.all ([axios.get(`https://api.github.com/users/${this.githubUserName}`),axios.get(`https://api.github.com/users/${this.githubUserName}/starred`)]
-)            .then(([{data:{ avatar_url, location, name, blog, bio, public_repos, followers, following,}}, {data}]) => {
+)            .then(([{data:{ avatar_url, location, name, blog, bio, public_repos, followers, following,}}, {data: {length}}]) => {
                 this.avatar_url = avatar_url;
                 this.location = location;
                 this.name = name;
@@ -41,15 +47,30 @@ class DoMyHomework {
                 this.public_repos = public_repos;
                 this.followers = followers;
                 this.following = following;
+                this.stars = length;
+                this.color = this.color;
                 // console.log(this);
-                console.log(data)
+                // console.log(length)
+                // console.log(this.color);
+                this.createHthml();
             })
     }
     createHthml(){
-
+        this.html = `
+        <html>
+        <body>
+        name: ${this.name}
+        </body>
+        </html>
+        `;
+        console.log(this)
+        this.createPdf();
     }
     createPdf(){
-
+        pdf.create(this.html).toFile('./gitHubProfile.pdf' , function(err, res){
+            if (err) return console.log(err);
+            console.log(res)
+        })
     }
 
 }
@@ -58,10 +79,6 @@ class DoMyHomework {
 var newHomework = new DoMyHomework();
 // console.log(newHomework);
 newHomework.promptUser();
-
-
-
-
 
 
 
